056d6c92d7a38606400d49052b71e2309c995f1b
Jim Seconde <jim.seconde@googlemail.com>
2021-07-31 16:21:10 +0100

    skeleton service class

---
+++ src/Application/Services/VonageApiService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Application\Services;
+
+class VonageApiService {
+
+    public string $keys;
+
+    public function __construct()
+    {
+        $this->keys = 'wiring test';
+    }
+}   
