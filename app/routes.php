c7a9fa3dc12a6a868a313df6d10d59c968ea6771
James Seconde <jim.seconde@googlemail.com>
2021-08-05 08:21:53 +0100

    Update routes.php
    Remove user handling, minimal Slim usage

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -29,10 +27,5 @@ return function (App $app) {
 
     $app->group('/sms', function (Group $group) {
         $group->post('/send', SendSmsHandler::class);
-    });
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
     });
 };
